5407bae2e717a60281b74aa78184045a32436293
Timerlan Chinchaev <tchinchaev@bk.ru>
2023-09-15 03:55:32 +0300

    make tg bot

--- my.py
+++ my.py
@@ -13,15 +13,50 @@ import threading
 from PyQt5.QtCore import QTimer, QDateTime, Qt
 import telebot
 import socket
+from PyQt5.QtCore import QMetaObject, Qt
+from PyQt5.QtCore import Qt, QTimer, QUrl, QObject, pyqtSignal
+from PyQt5.QtCore import Qt, QTimer, QUrl, QObject, pyqtSignal, QMetaObject, Q_ARG
+
+try:
+    token = '6329551488:AAHLaNLS9jLjdFknA7DbECkmWI1jEDhXoDA'
+    bot=telebot.TeleBot(token)
+except:
+    print('Нет доступа к сети')
 
 
 class AdvertisementPlayer(QMainWindow):
+
+
     def __init__(self):
         super().__init__()
+        # Проверяем наличие папки "media" и создаем её, если она отсутствует
+        media_folder = 'media'
+        if not os.path.exists(media_folder):
+            os.makedirs(media_folder)
+        self.schedule_disk_check()
         self.initUI()
         self.schedule_disk_check()
+        
+    def run_media_timer(self):
+        self.media_timer.timeout.connect(self.show_next_media)
+        self.media_timer.start(self.interval * 1000)
 
     def initUI(self):
+        self.playback_timer_interval = 60 * 1000  # в миллисекундах (1 минута)
+        self.last_playback_time = QDateTime.currentDateTime()
+        self.interval = 3  # Интервал по умолчанию в секундах
+        self.playing = False  # Флаг для отслеживания воспроизведения
+        self.player_started = False  # Флаг для отслеживания команды /start
+
+        self.media_timer = QTimer(self)  # Создайте QTimer для медиа-таймера
+        self.media_timer.timeout.connect(self.show_next_media)
+
+                # Create a timer for changing media based on the selected interval
+        self.playback_timer = QTimer(self)
+        self.playback_timer.timeout.connect(self.check_playback_status)
+        self.playback_timer.start(self.playback_timer_interval)
+        self.last_playback_time = QDateTime.currentDateTime()
+        # Оставляем только QLabel для отображения медиаматериала
         self.setWindowTitle('Плеер рекламы')
         self.setGeometry(0, 0, 800, 600)
 
@@ -30,90 +65,38 @@ class AdvertisementPlayer(QMainWindow):
 
         layout = QVBoxLayout()
 
-        # Create a label for displaying media (image or video)
         self.media_label = QLabel(self)
         self.media_label.setAlignment(Qt.AlignCenter)
         layout.addWidget(self.media_label)
 
-        # Create a toolbar
-        self.create_toolbar()
-        self.playback_timer_interval = 60 * 1000  # в миллисекундах (1 минута)
-        self.last_playback_time = QDateTime.currentDateTime()
-        # Create a line edit for inputting the interval (in seconds) between media changes
-        self.interval_edit = QLineEdit(self)
-        self.interval_edit.setPlaceholderText('Введите интервал в секундах')
-        layout.addWidget(self.interval_edit)
-
-        # Set the layout in the main widget
         self.central_widget.setLayout(layout)
 
-
-        self.last_playback_time = QDateTime.currentDateTime()
-        
-
-        self.telegram_bot_token = '6329551488:AAHLaNLS9jLjdFknA7DbECkmWI1jEDhXoDA'
-        self.bot = telebot.TeleBot(token=self.telegram_bot_token)
-
-
-        # Create a timer for changing media based on the selected interval
-        self.playback_timer = QTimer(self)
-        self.playback_timer.timeout.connect(self.check_playback_status)
-
-                # Создайте таймер для автоматической смены медиафайлов
-        self.media_timer = QTimer(self)
-        self.media_timer.timeout.connect(self.show_next_media)
-
-        # Начните таймер при старте приложения
-        self.playback_timer.start(self.playback_timer_interval)
-
-        # Initialize the list of media files from the "media" folder
         self.media_files = self.load_media_files("media")
-
-        # Index of the current media
         self.current_media_index = 0
 
-        # Initialize the media player and video widget
         self.media_player = None
         self.video_widget = None
         self.video_playing = False
-        self.video_stopped = False  # Добавлено флаг для отслеживания окончания видео
-        self.auto_change_interval = 0  # Добавьте переменную для хранения интервала автоматической смены
-
-        # Show the first media
-        self.show_media(self.current_media_index)
-
-
-
-    def create_toolbar(self):
-        self.toolbar = QWidget(self)
-        self.toolbar.setGeometry(10, 10, 200, 50)
-
-        # Button to start automatic media change
-        self.start_button = QPushButton('Старт', self.toolbar)
-        self.start_button.setGeometry(0, 0, 80, 30)
-        self.start_button.clicked.connect(self.start_media_change)
-
-        # Button to stop automatic media change
-        self.stop_button = QPushButton('Стоп', self.toolbar)
-        self.stop_button.setGeometry(90, 0, 80, 30)
-        self.stop_button.clicked.connect(self.stop_media_change)
+        self.video_stopped = False
+        self.auto_change_interval = 0
+        try:
+            self.telegram_bot_token = '6329551488:AAHLaNLS9jLjdFknA7DbECkmWI1jEDhXoDA'
+            self.bot = telebot.TeleBot(token=self.telegram_bot_token)
+        except:
+            print('Нет досутпа к сети')
 
     def load_media_files(self, directory):
-        # Get a list of files in the specified folder
         media_files = []
         for filename in os.listdir(directory):
             if filename.endswith(('.jpg', '.jpeg', '.png', '.gif', '.mp4', '.avi', '.mkv')):
                 media_files.append(os.path.join(directory, filename))
         return media_files
 
-        
     def show_media(self, index):
         if 0 <= index < len(self.media_files):
             media_file = self.media_files[index]
             if media_file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif')):
-                # Display images or GIFs
                 if self.media_player:
-                    # If a video is playing, stop it and remove the video widget
                     self.media_player.stop()
                     self.central_widget.layout().removeWidget(self.video_widget)
                     self.video_widget.deleteLater()
@@ -121,52 +104,48 @@ class AdvertisementPlayer(QMainWindow):
                     self.media_player = None
 
                 pixmap = QPixmap(media_file)
-
-                # Scale the pixmap to fit the QLabel
                 pixmap = pixmap.scaled(self.media_label.size(), Qt.KeepAspectRatio)
-
                 self.media_label.setPixmap(pixmap)
-                self.video_playing = False  # Set video_playing to False for images
-                self.video_stopped = True  # Установить флаг окончания видео в True
-                # Disconnect the mediaPlayerStateChanged signal if it was connected
-                if self.media_player:
-                    self.media_player.stateChanged.disconnect(self.media_player_state_changed)
+                self.video_playing = False
+                self.video_stopped = True
+
             elif media_file.lower().endswith(('.mp4', '.avi', '.mkv')):
-                # Play videos
                 if not self.media_player:
-                    # If no media player exists, create one
                     self.media_player = QMediaPlayer()
                     self.video_widget = QVideoWidget()
                     self.media_player.setVideoOutput(self.video_widget)
                     self.central_widget.layout().addWidget(self.video_widget)
 
-                # Set the media content and play the video
                 self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(media_file)))
-                self.video_widget.setFullScreen(True)  # Display video in fullscreen
+                self.video_widget.setFullScreen(True)
                 self.media_player.play()
-                self.video_playing = True  # Set video_playing to True for videos
-                self.video_stopped = False  # Установить флаг окончания видео в False
-                # Connect the mediaPlayerStateChanged signal to the handler
+                self.video_playing = True
+                self.video_stopped = False
                 self.media_player.stateChanged.connect(self.media_player_state_changed)
-                # Остановить таймер перед воспроизведением видео
                 self.media_timer.stop()
             else:
                 self.media_label.setText("Неподдерживаемый формат файла")
 
             self.last_playback_time = QDateTime.currentDateTime()
 
+    def start_media_timer(self):
+        QMetaObject.invokeMethod(self.media_timer, "start", Qt.QueuedConnection, Q_ARG(int, self.interval * 1000))
+
+    def stop_media_timer(self):
+        QMetaObject.invokeMethod(self.media_timer, "stop", Qt.QueuedConnection)
+
+
 
     def show_next_media(self):
-        # Switch to the next media
         self.current_media_index += 1
         if self.current_media_index >= len(self.media_files):
             self.current_media_index = 0
         self.show_media(self.current_media_index)
-    
+
 
     def start_media_change(self):
         # Start changing media automatically based on the selected interval
-        interval = int(self.interval_edit.text())
+        interval = self.media_timer
         self.auto_change_interval = interval  # Сохраните интервал
 
         if not self.video_playing:  # Only start the timer if video is not playing
@@ -178,11 +157,14 @@ class AdvertisementPlayer(QMainWindow):
         self.download_all_files_from_folder(yandex_disk_folder, save_folder)
 
 
+
     def stop_media_change(self):
         # Stop changing media
         self.media_timer.stop()
 
-        
+       
+
+
 
     def media_player_state_changed(self, state):
         # Handler for mediaPlayerStateChanged signal
@@ -199,6 +181,7 @@ class AdvertisementPlayer(QMainWindow):
                 self.video_playing = False  # Устанавливаем флаг video_playing в False
                 interval = self.auto_change_interval
                 self.media_timer.start(interval * 1000)
+                
   
         elif state == QMediaPlayer.PlayingState:
             # Video playback has started, stop the interval timer
@@ -234,7 +217,10 @@ class AdvertisementPlayer(QMainWindow):
 
     def schedule_disk_check(self):
         # Расписание для проверки Яндекс.Диска каждые 30 минут
-        schedule.every(2).minutes.do(self.check_yandex_disk)
+        try:
+            schedule.every(2).minutes.do(self.check_yandex_disk)
+        except:
+            print('Нет доступа к сети')
 
     def check_yandex_disk(self):
 
@@ -290,13 +276,65 @@ class AdvertisementPlayer(QMainWindow):
         current_time = QDateTime.currentDateTime()
         time_difference = self.last_playback_time.secsTo(current_time)
 
-        if time_difference > 30:  # 300 секунд = 5 минут
+        if time_difference > 5 and not self.video_playing:  # 300 секунд = 5 минут
             # Если время безуспешного воспроизведения превышает 5 минут, отправьте уведомление в Telegram
             chat_id = '5455171373'  # Замените на ID вашего чата или пользователя
             message = 'Воспроизведение не работает в течение более 5 минут на компьютере.'
             self.send_telegram_notification(chat_id, message)
 
+    def set_interval(self, interval):
+            # Функция для установки интервала через Telegram бота
+        self.interval = interval
+        self.media_timer.stop()
+        self.media_timer.start(interval * 1000)
 
+    def start_playback(self):
+            # Функция для старта воспроизведения через Telegram бота
+        self.playing = True
+        self.auto_change_interval = self.interval
+        if not self.video_playing:  # Only start the timer if video is not playing
+            self.media_timer.start(self.interval * 1000)  # Convert to milliseconds
+        yandex_disk_folder = '/media'  # Путь к папке на Яндекс.Диске
+        save_folder = 'media/'  # Путь к папке на вашем компьютере
+        try:
+            self.download_all_files_from_folder(yandex_disk_folder, save_folder)
+        except:
+            print('Нет доступа к сети')
+
+    def stop_playback(self):
+            # Функция для остановки воспроизведения через Telegram бота
+        self.playing = False
+        self.media_timer.stop()
+
+
+def run_bot_thread():
+    # Обработчики команд для Telegram бота
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        # Обработчик команды /start
+        window.player_started = True  # Set the player started flag to True
+        window.start_playback()
+        window.start_media_timer()  # Start the media timer
+        bot.send_message(message.chat.id, 'Воспроизведение медиаматериала начато.')
+
+    @bot.message_handler(commands=['stop'])
+    def stop(message):
+        # Обработчик команды /stop
+        window.stop_playback()
+        window.stop_media_timer()  # Stop the media timer
+        bot.send_message(message.chat.id, 'Воспроизведение медиаматериала остановлено.')
+
+    @bot.message_handler(commands=['set_interval'])
+    def set_interval(message):
+        # Обработчик команды /set_interval
+        try:
+            interval = int(message.text.split()[-1])
+            window.set_interval(interval)
+            bot.send_message(message.chat.id, f'Интервал установлен на {interval} секунд.')
+        except ValueError:
+            bot.send_message(message.chat.id, 'Некорректное значение интервала.')
+
+    bot.polling()
 
 def run_schedule():
     while True:
@@ -308,10 +346,23 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = AdvertisementPlayer()
     window.showFullScreen()
-    
-    # Создайте и запустите отдельный поток для выполнения расписания
+    try:
+        bot = telebot.TeleBot(token='6329551488:AAHLaNLS9jLjdFknA7DbECkmWI1jEDhXoDA')  # Replace with your actual bot token
+    except:
+        print('Нет досутпа к сети')
+
+        # Создайте и запустите отдельный поток для выполнения расписания
     schedule_thread = threading.Thread(target=run_schedule)
     schedule_thread.daemon = True  # Поток завершится, когда завершится главный поток
     schedule_thread.start()
-    
+    try:
+        # Start the Telegram bot thread
+        bot_thread = threading.Thread(target=run_bot_thread)
+        bot_thread.daemon = True
+        bot_thread.start()
+    except:
+        print('Нет досутпа к сети')
+
+
+
     sys.exit(app.exec_())
